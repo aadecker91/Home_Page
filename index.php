646a11904b747d0c593b0d9a435b0065ecf76901
Adam Decker <aadecker91@gmail.com>
2015-01-13 18:20:56 -0500

    First Commit

---
+++ index.php
@@ -0,0 +1,48 @@
+#!/usr/local/bin/php
+
+<html>
+<head>
+<meta charset="utf-8">
+<title>Adam Decker</title>
+
+<link href="_CSS/styleSheet.css" rel="stylesheet" type="text/css" />
+<link href="SpryAssets/SpryMenuBarHorizontal.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+.content {
+	width: 98%;
+	background-color: rgba(0,0,255,.3);
+	margin-left: 1%;
+	border-radius: 5px;
+	padding: 10px 0;
+}
+.header {
+	text-align: left;
+}
+</style>
+<script src="SpryAssets/SpryMenuBar.js" type="text/javascript"></script>
+<meta name="description" content="Adam Decker, Computer Science Master's Student, home page and portfolio.  Contains links to UF Club Tennis Information, PHPoor Security, and TA information." />
+<meta name="keywords" content="Adam Decker, UF, Home, University of Florida, UF Club Tennis, " />
+</head>
+
+<body>
+
+<div class="container">
+  <div class="header">
+	  <img src="_images/Profile_Picture.jpg" alt="Profile" width="156" height="240"/> <p>Adam Decker</p><p>Adam Decker</p><p>Adam Decker</p><p>Adam Decker</p>
+  <?php include("_includes/header.php"); ?>
+  <!-- end .header --></div>
+  <h1>Home Page</h1>
+  <div class="content">
+    <p>Welcome to my webpage!</p>
+    <p>&nbsp;</p>
+    <p>This website contains link to some of my projects/UF Club Tennis Website/TA classes/etc.</p>
+  </div>
+  <div id="body"><!-- body div for footer --></div>
+  <div class="footer">Adam Decker
+  <!-- end .footer --></div>
+  <!-- end .container --></div>
+<script type="text/javascript">
+<?php include("_includes/menuBar1.php"); ?>
+</script>
+</body>
+</html>
